fbea59e4ec332ad623c0213ca0c1543e98381c1c
lukendatigh <lukendatigh@gmail.com>
2019-11-05 23:45:06 +0100

    add event views

--- src/events/views.py
+++ src/events/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import (
+	ListView,
+	DetailView,
+	)
 
-# Create your views here.
+from . models import Event
+
+
+class EventListView(ListView):
+	model = Event
+	context_page_name = 'events'
+	paginate_by = 6
+	template_name = 'events/event_list.html'
+
+
+class EventDetailView(DetailView):
+	model = Event
+	context_page_name = 'event'
+	template_name = 'events/event_content.html'
